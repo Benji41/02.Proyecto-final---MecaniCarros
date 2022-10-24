54c06dde90c88a91ab51d6f6f3b63d9d4995159c
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-10-24 15:48:24 -0700

    Commit 10/24/22

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ $(".btnClose").click(
  * GRID CATEGORIAS
  * 
  */
-$(".grid figure").mouseover(
+$(".grid figure, .gridFooter figure").mouseover(
     function () { 
         $(this).css({
             "background-position":"right bottom"
@@ -60,13 +60,23 @@ $(".grid figure").mouseover(
     }
 )
 
-$(".grid figure").mouseout(
+$(".grid figure , .gridFooter figure").mouseout(
     function () { 
         $(this).css({
             "background-position":"left top"
         })
     }
 )
+
+$(".grid figure").click(function () { 
+    var vinculo = $(this).attr("vinculo");
+    window.location = vinculo;
+ })
+
+ $(".gridFooter figure").click(function () { 
+    var vinculo = $(this).attr("vinculo");
+    window.location = vinculo;
+ })
 
 /**PAGINACION */
 $(".pagination").twbsPagination({
@@ -77,3 +87,77 @@ $(".pagination").twbsPagination({
     prev: '<i class="fas fa-angle-left"></i>',
     next: '<i class="fas fa-angle-right"></i>'
 })
+
+
+//SCROLORAMA
+
+var controller  = $.superscrollorama();
+controller.addTween(".contenidoInicio .container", TweenMax.from(
+    $(".contenidoInicio .container"), .5,{css:{opacity:0}}
+))
+
+//SCROLL UP
+$.scrollUp({
+    scrollText:"",
+    scrollSpeed: 2000,
+    easingType: "easeOutQuint"
+})
+/* PRELOAD */
+$("body").css({
+    "overflow-y":"hidden"
+})
+
+var cargarImg = $("img");
+var cargarScript = $("script");
+var cargarCSS = $("link");
+var cargarVideos = $("video");
+var cargarAudios = $("audio");
+var valorPorcentaje =0
+var incremento =0;
+var numCarga =0;
+var totalObjects = [];
+var numItem=0 ;
+
+totalObjects.push(cargarAudios,cargarCSS,cargarImg,cargarScript,cargarVideos)
+//console.log(totalObjects);
+
+totalObjects.forEach(functionForEach)
+
+function functionForEach(item,index) { 
+    for (let index = 0; index < item.length; index++) {
+        numItem++
+
+        valorPorcentaje = 100 / numItem
+        console.log(`valor porcentaje : ${valorPorcentaje}`);
+    }
+
+    for (let index = 0; index < item.length; index++) {
+        preload(index,item)
+    }
+   
+ }
+ function preload(index,item) {
+    setTimeout(function () { 
+        $(item[index]).ready(function(){
+            numCarga++;
+            incremento = Math.floor(numCarga * valorPorcentaje);
+
+            $("#porcentajeCarga").html(incremento+"%");
+
+            $("#rellenoCarga").css({
+                "width": incremento+"%"
+            });
+
+            if(incremento >= 100){
+                $("#preload").delay(350).fadeOut("slow");
+                $("body").delay(350).css(
+                    {
+                        "overflow-y" : "scroll"
+                    }
+                )
+            }
+        })
+    },index*100)
+}
+
+//console.log(`Img :${cargarImg} script: ${cargarScript} css: ${cargarCSS} videos: ${cargarVideos} audios: ${cargarAudios}`);
